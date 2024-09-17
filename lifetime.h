9b6fb34a51a9e36a93864e0fbd9d0a2d831ad90b
duoduo70 <81518666+duoduo70@users.noreply.github.com>
2024-09-17 17:25:43 +0800

    Create lifetime.h

---
+++ lifetime.h
@@ -0,0 +1,44 @@
+#ifndef _LIFETIME_H_
+#define _LIFETIME_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define lftm(__size, __block)                                                  \
+        do {                                                                   \
+                __lftm_start(__size);                                          \
+                do                                                             \
+                        __block while (0);                                     \
+                __lftm_end();                                                  \
+        } while (0);
+
+#define lftm_export(__ptr) __lftmmap_export(lftm_stack_last(), __ptr)
+
+enum PointerMapType {
+        LFTMMAP_TYPE_HASHMAP,
+        LFTMMAP_TYPE_LINKLIST,
+        LFTMMAP_TYPE_VLA
+};
+
+struct PointerMap {
+        size_t hashmapsize; // hashmapsize == 0 <==> typeof map == LINKLIST
+                            // hashmapsize == -1 <==> typeof map == VLA
+        void **map;
+};
+
+struct PointerMap lftmmap_init(size_t maxsize);
+void __lftmmap_free(struct PointerMap map);
+
+struct PointerMap lftm_stack_last();
+
+enum PointerMapType lftmmap_gettype(struct PointerMap *map);
+void lftmmap_insert(struct PointerMap *map, void *ptr);
+void lftmmap_foreach(struct PointerMap *map, void (*f)(void *));
+
+void *lftm_malloc(size_t size);
+void __lftmmap_export(struct PointerMap map, void *ptr);
+
+void __lftm_start(size_t maxsize);
+void __lftm_end();
+
+#endif
